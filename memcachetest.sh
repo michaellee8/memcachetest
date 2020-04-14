06b396dfb9d0b69d138cbf7dcaf122806534db75
michaellee8 <ckmichael8@gmail.com>
2020-04-14 20:48:57 +0800

    Update memcachetest.sh

--- memcachetest.sh
+++ memcachetest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(hostname && echo && cat /proc/cpuinfo && echo && getconf -a | grep CACHE && echo && lscpu && echo && ./mbench -c ) > log.txt
+(wget -q https://michaellee8.github.io/memcachetest/mbench && chmod +x ./mbench && hostname && echo && cat /proc/cpuinfo && echo && getconf -a | grep CACHE && echo && lscpu && echo && ./mbench -c ) > log.txt
 curl -X POST 'https://demo5256874.mockable.io/test-result' --data-binary @log.txt -H 'Content-Type: text/plain'
